095adefbad1e6f69fb0c563c398f5cc254d0b7c7
Eugene Che <indexpro24@gmail.com>
2020-05-10 21:02:25 +0300

    Git Initial

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: {color: 'red'},
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/node_modules/bootstrap/dist/css/bootstrap.min.css',
+    // '@/node_modules/font-awesome/scss/font-awesome'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
